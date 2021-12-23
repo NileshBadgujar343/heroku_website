6abadee4608be4c7356d049fa9881acea42cea70
Liyakat Shaikh <liyakatshaikh07@gmail.com>
2021-12-23 14:19:21 +0530

    greenair backend init

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "love@123",
+    DB: "bhel"
+  };
---
+++ app/routes/sensor.routes.js
@@ -0,0 +1,24 @@
+module.exports = app => {
+    const sensors = require("../controllers/sensor.controller.js");
+  
+    // Create a new Product
+    app.post("/users/register", sensors.create);
+  
+    // Retrieve all Products
+    app.get("/sensors/:tableId", sensors.findAll);
+
+    // Retrieve all Users
+    app.post("/users/:userId", sensors.findAll);
+  
+    // Retrieve a single Product with sensorId
+    app.get("/sensors/:sensorId", sensors.findOne);
+  
+    // Update a Product with sensorId
+    app.put("/sensors/:sensorId", sensors.update);
+  
+    // Delete a Product with sensorId
+    app.delete("/sensors/:sensorId", sensors.delete);
+  
+    // Create a new Product
+    app.delete("/sensors", sensors.deleteAll);
+  };
